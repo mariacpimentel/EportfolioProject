656dcf31f7759b5524de7f537b155810a6a4fd86
Maria Clara Pimentel <macla@Mac.lan>
2025-12-12 10:24:46 -0500

    html commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//template_s7s4nv8
+//service_ap9e0yh
+//Z6IorspwV2q02iuHL
+
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if(contrastToggle){
+        document.body.classList += " dark-theme";
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact(event){
+event.preventDefault()
+const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
+loading.classList += " modal__overlay--visible";
+emailjs
+    .sendForm(
+        'service_ap9e0yh',
+        'template_s7s4nv8',
+        event.target,
+        'Z6IorspwV2q02iuHL'
+    ).then(()=>{
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    }).catch(()=>{
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on maclarealtor@gmail.com"
+        );
+    })
+}
+
+let isModalOpen = false;
+function toggleModal(){
+    if (isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
